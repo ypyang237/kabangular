def341962bae9341fc99b8072956f5645abb8e00
ypyang237 <ypyang237@gmail.com>
2016-05-05 10:51:51 -1000

    sets up basic folders

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
