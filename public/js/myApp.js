d288fe53dd6c7d19b8762479935a446d33b14fbd
tpyoung <tylerparkeyoung@gmail.com>
2016-05-05 11:36:27 -1000

    initial hello world working

---
+++ public/js/myApp.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function() {
+  angular.module('myApp', ['ngRoute']);
+
+var myApp = angular.module('myApp');
+
+myApp
+  .config(['$locationProvider', '$routeProvider',function($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false}
+      );
+    $routeProvider
+      .when('/', {
+        templateUrl : 'views/default.html'
+      })
+      .otherwise({
+        templateUrl : 'views/error404.html',
+        controller  : 'ErrorController' });  }])
+  .run(['$rootScope', function($rootScope){
+    // run information
+  }]);
+})();
--- server.js
+++ server.js
@@ -6,5 +6,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
+app.get('*', function(req, res){
+  res.sendFile('./public/index.html',
+    {
+      root  : __dirname
+    });
+});
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
